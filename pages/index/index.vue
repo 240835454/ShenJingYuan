5e89fa41fdf66d0895d3b9612e184da07e36114a
TXF <240835454@qq.com>
2019-11-01 18:08:09 +0800

    2019-11-1

---
+++ pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+			<button @click='request'>测试</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			uni.setNavigationBarTitle({
+				title: '哈哈'
+			})
+		},
+		methods: {
+			request(){
+				wx.request({
+					url: 'http://192.168.1.199:8359/API/AreaInfo/GetAreaInfo?ParentId=0',
+					success: res=>{
+						console.log(res);
+					}
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
